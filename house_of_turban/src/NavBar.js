3ac1011b5ad6b9a816c355f1d0fc4ad1bba5ba6a
evial98 <paraalva@ee.duth.gr>
2020-08-25 23:24:52 +0300

    ContactUs Draft

--- house_of_turban/src/NavBar.js
+++ house_of_turban/src/NavBar.js
@@ -14,6 +14,7 @@ class NavBar extends Component {
         <Link to='/Login' className="nav-link">My Account</Link>
         <Link to='/Register' className="nav-link">Register</Link>
         <Link to='/AllProducts' className="nav-link">Products</Link>
+        <Link to='ContactUs' className="nav-link">Contact Us</Link>
 
       </Nav>
       <Form inline>
